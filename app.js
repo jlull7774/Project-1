be42fe36245440572bba6de2145678a2ee3d881f
Jason Lull <jlull7774@gmail.com>
2022-05-24 13:51:20 -0400

    first js started

---
+++ app.js
@@ -0,0 +1,7 @@
+const line1 = document.getElementById("line1");
+const line2 = document.getElementById("line2");
+const line3 = document.getElementById("line3");
+
+function randomItem() {
+  return Math.floor(Math.random() * 10) + 1;
+}
