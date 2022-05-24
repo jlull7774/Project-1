08d5e35a9590f361dd950498bf955e9a538b374d
Jason Lull <jlull7774@gmail.com>
2022-05-24 16:34:30 -0400

    90% final product

--- app.js
+++ app.js
@@ -17,60 +17,127 @@ songs[3] = "Luv My Slatts";
 songs[4] = "Already Rich";
 
 function outOfTimeSong() {
-  let audio1 = new Audio("insert mp3");
-  audio1.play;
+  let audio1 = document.getElementById("out-of-time");
+  audio1.play();
 }
 function loveSoRight() {
-  let audio2 = new Audio("insert mp3");
-  audio2.play;
+  let audio2 = document.getElementById("love-so-right");
+  audio2.play();
 }
 function aroundTheWorld() {
-  let audio3 = new Audio("insert mp3");
-  audio3.play;
+  let audio3 = document.getElementById("around-the-world");
+  audio3.play();
 }
 function luvMySlatts() {
-  let audio4 = new Audio("insert mp3");
-  audio4.play;
+  let audio4 = document.getElementById("luv-my-slatts");
+  audio4.play();
 }
 function alreadyRich() {
-  let audio5 = new Audio("insert mp3");
-  audio5.play;
+  let audio5 = document.getElementById("already-rich");
+  audio5.play();
 }
 
 function randomItem() {
   return Math.floor(Math.random() * songs.length) + 1;
 }
+function spinTheMachine1() {
+  setTimeout(() => {
+    let line1 = document.getElementById("line1");
+    //   let line2 = document.getElementById("line2");
+    //   let line3 = document.getElementById("line3");
 
-function spinTheMachine() {
-  let line1 = document.getElementById("line1");
-  let line2 = document.getElementById("line2");
-  let line3 = document.getElementById("line3");
+    let item1 = randomItem();
+    // let item2 = randomItem();
+    // let item3 = randomItem();
 
-  let item1 = randomItem();
-  let item2 = randomItem();
-  let item3 = randomItem();
+    line1.innerHTML = item1;
+    //   line2.innerHTML = item2;
+    //   line3.innerHTML = item3;
 
-  line1.innerHTML = item1;
-  line2.innerHTML = item2;
-  line3.innerHTML = item3;
-
-  if (item1 == 1 || item2 == 1 || item3 == 1) {
-    console.log(songs[0]);
-  }
-  if (item1 == 2 || item2 == 2 || item3 == 2) {
-    console.log(songs[1]);
-  }
-  if (item1 == 3 || item2 == 3 || item3 == 3) {
-    console.log(songs[2]);
-  }
-  if (item1 == 4 || item2 == 4 || item3 == 4) {
-    console.log(songs[3]);
-  }
-  if (item1 == 5 || item2 == 5 || item3 == 5) {
-    console.log(songs[4]);
-  }
+    if (item1 == 1) {
+      outOfTimeSong();
+    }
+    if (item1 == 2) {
+      loveSoRight();
+    }
+    if (item1 == 3) {
+      aroundTheWorld();
+    }
+    if (item1 == 4) {
+      luvMySlatts();
+    }
+    if (item1 == 5) {
+      alreadyRich();
+    }
+  }, 5000);
 }
+function spinTheMachine2() {
+  setTimeout(() => {
+    //   let line1 = document.getElementById("line1");
+    let line2 = document.getElementById("line2");
+    //   let line3 = document.getElementById("line3");
 
+    //   let item1 = randomItem();
+    let item2 = randomItem();
+    // let item3 = randomItem();
+
+    //   line1.innerHTML = item1;
+    line2.innerHTML = item2;
+    //   line3.innerHTML = item3;
+
+    if (item2 == 1) {
+      outOfTimeSong();
+    }
+    if (item2 == 2) {
+      loveSoRight();
+    }
+    if (item2 == 3) {
+      aroundTheWorld();
+    }
+    if (item2 == 4) {
+      luvMySlatts();
+    }
+    if (item2 == 5) {
+      alreadyRich();
+    }
+  }, 10000);
+}
+function spinTheMachine3() {
+    setTimeout(() => {
+      //   let line1 = document.getElementById("line1");
+    //   let line2 = document.getElementById("line2");
+        let line3 = document.getElementById("line3");
+  
+      //   let item1 = randomItem();
+    //   let item2 = randomItem();
+      let item3 = randomItem();
+  
+      //   line1.innerHTML = item1;
+    //   line2.innerHTML = item2;
+        line3.innerHTML = item3;
+  
+      if (item3 == 1) {
+        outOfTimeSong();
+      }
+      if (item3 == 2) {
+        loveSoRight();
+      }
+      if (item3 == 3) {
+        aroundTheWorld();
+      }
+      if (item3 == 4) {
+        luvMySlatts();
+      }
+      if (item3 == 5) {
+        alreadyRich();
+      }
+    }, 15000);
+  }
+function masterSpin() {
+  spinTheMachine1();
+  spinTheMachine2();
+  spinTheMachine3();
+}
 // console.log(item1, item2, item3);
 // randomItem();
 // console.log(randomItem());
